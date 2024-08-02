dd92c22283f54955b28792a81e4030cd0502f7f7
joe shmo <omicron@omicron.home>
2024-08-02 15:32:58 -0500

    add ip address table entry

--- IpAddress.java
+++ IpAddress.java
@@ -1,19 +1,26 @@
-import java.util.Random;
+import java.util.*;
 import java.lang.*;
+import java.io.*;
 
 public class IpAddress{
-    public static String generateIP(){
+    public static String generate_IP(){
         Random rand = new Random();
         int octet;
-        String IP = "hellow";
+        String IP = "";
         for (int i = 0; i < 4; i++){
             octet = rand.nextInt(256);
-            IP.concat(Integer.toString(octet));
+            IP += Integer.toString(octet);
             if(i != 3){
-                IP.concat(".");
+                IP += ".";
             }
         }
-        IP = "wasssup";
         return IP;
+    }
+    public static String assign_IP(String host){
+        ArrayList<String> taken_IPs = new ArrayList<>();
+        String assigned_IP = IpAddress.generate_IP();
+        String entry = host + " : " + assigned_IP;
+        taken_IPs.add(entry);
+        return assigned_IP;
     }
 }
--- host.java
+++ host.java
@@ -1,6 +1,6 @@
 public class host {
        
-    String mac = "AD-BC-23-45-D9-D2";// Same with MAC Address
-    String ipv4 = IpAddress.generateIP(); 
+    String mac = "AD-BC-23-45-D9-D3";// Same with MAC Address
+    String ipv4 = IpAddress.assign_IP("test"); 
     
 }
