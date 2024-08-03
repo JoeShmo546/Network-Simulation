d17231d1d3aefbcd2c83c5beff7bb18aac2958c3
joe shmo <omicron@omicron.home>
2024-08-03 11:39:28 -0500

    rename strings, fix unique ip checker

--- IpAddress.java
+++ IpAddress.java
@@ -3,7 +3,8 @@ import java.lang.*;
 import java.io.*;
 
 public class IpAddress{
-    public static String generate_IP(){
+
+    public static String generateIP(){
         Random rand = new Random();
         int octet;
         String IP = "";
@@ -16,11 +17,47 @@ public class IpAddress{
         }
         return IP;
     }
-    public static String assign_IP(String host){
-        ArrayList<String> taken_IPs = new ArrayList<>();
-        String assigned_IP = IpAddress.generate_IP();
-        String entry = host + " : " + assigned_IP;
-        taken_IPs.add(entry);
-        return assigned_IP;
+    // class assures that IP addresses cannot be identical
+    public static String assignIP(String host){
+
+        ArrayList<String> takenIPs = new ArrayList<>();
+        String separator = ": ";
+        String assignedIP = "";
+        int separatorIndex; 
+        boolean isUnique = false;
+        
+        /* generate an IP Address
+         * cycle through list of IP Addresses
+         * check if the generated IP Address is equal to current IP Address in the cycle
+         * if it is equal start over and generate a new IP Address 
+         * this loop is infinite
+         */
+        while (!isUnique){
+            assignedIP = IpAddress.generateIP();
+
+            if (takenIPs.size() > 0){
+
+                for (int i = 0; i < takenIPs.size(); i++){
+                    String currentEntry = takenIPs.get(i);
+                    separatorIndex = currentEntry.indexOf(separator);
+                    String entryIP = currentEntry.substring(separatorIndex + separator.length());
+
+                    if (assignedIP.equals(entryIP)){
+                        break;
+                    } else if (i == takenIPs.size()){
+                        isUnique = true;
+                        break;
+                    }
+                }
+
+            } else {
+                isUnique = true;
+            }
+        }
+
+        String entry = host + " : " + assignedIP;
+        takenIPs.add(entry);
+        System.out.println(takenIPs);
+        return assignedIP;
     }
 }
